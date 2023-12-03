8e9945542e87a00379068a2c81463cfd5944ea0e
halkner <halkner77@hotmail.com>
2023-12-03 18:34:27 -0300

    :recycle: makes nomenclature clearer

--- client.c
+++ client.c
@@ -4,44 +4,44 @@
 #include <Winsock2.h>
 #include <ws2tcpip.h>
 
+#define BUFFER_SIZE 1024
+#define IP_ADDRESS "127.0.0.1"
+#define PORT_NUMBER 3300
+
 int main()
 {
-    char *ip = "127.0.0.1";
-    int port = 3300;
-
-    int sock;
-    struct sockaddr_in addr;
-    socklen_t addr_size;
-    char buffer[1024];
-    int n;
-
     WSADATA wsaData;
+    SOCKET sock;
+    struct sockaddr_in addr;
+    socklen_t addrSize;
+    char buffer[BUFFER_SIZE];
+
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        perror("[-]WSAStartup failed");
-        exit(1);
+        perror("[-] WSAStartup failed");
+        exit(EXIT_FAILURE);
     }
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == INVALID_SOCKET)
     {
-        perror("[-]Socket error");
+        perror("[-] Socket error");
         WSACleanup();
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     printf("[+] TCP client socket created.\n");
 
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(port); // Convert port to network byte order
-    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(PORT_NUMBER);
+    addr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
 
     if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == SOCKET_ERROR)
     {
-        perror("[-]Connect error");
+        perror("[-] Connect error");
         closesocket(sock);
         WSACleanup();
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     printf("Connected to the server.\n");
 
@@ -57,7 +57,7 @@ int main()
     printf("Server: %s\n", buffer);
 
     closesocket(sock);
-    printf("Disconnected from the server. \n");
+    printf("Disconnected from the server.\n");
 
     WSACleanup();
 
--- server.c
+++ server.c
@@ -1,84 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <Winsock2.h>
 #include <ws2tcpip.h>
 
+#define BUFFER_SIZE 1024
+#define IP_ADDRESS "127.0.0.1"
+#define PORT_NUMBER 3300
+
 int main()
 {
-
-    char *ip = "127.0.0.1";
-    int port = 3300;
-
-    SOCKET server_sock, client_sock;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_size;
-    char buffer[1024];
-    int n;
-
     WSADATA wsaData;
+    SOCKET serverSock, clientSock;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t addrSize;
+    char buffer[BUFFER_SIZE];
+
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        perror("[-]WSAStartup failed");
-        exit(1);
+        perror("[-] WSAStartup failed");
+        exit(EXIT_FAILURE);
     }
 
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock == INVALID_SOCKET)
+    serverSock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSock == INVALID_SOCKET)
     {
-        perror("[-]Socket error");
-        exit(1);
+        perror("[-] Socket error");
+        exit(EXIT_FAILURE);
     }
     printf("[+] TCP server socket created.\n");
 
-    memset(&server_addr, '\0', sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port); // Convert port to network byte order
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT_NUMBER);
+    serverAddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
 
-    n = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (n == SOCKET_ERROR)
+    if (bind(serverSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
     {
-        perror("[-]Bind error");
-        closesocket(server_sock);
+        perror("[-] Bind error");
+        closesocket(serverSock);
         WSACleanup();
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    printf("[+] Bind to the port number: %d\n", port);
+    printf("[+] Bind to the port number: %d\n", PORT_NUMBER);
 
-    listen(server_sock, 5);
+    listen(serverSock, 5);
     printf("Listening...\n");
 
     while (1)
     {
-        addr_size = sizeof(client_addr);
-        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_size);
-        if (client_sock == INVALID_SOCKET)
+        addrSize = sizeof(clientAddr);
+        clientSock = accept(serverSock, (struct sockaddr *)&clientAddr, &addrSize);
+        if (clientSock == INVALID_SOCKET)
         {
-            perror("[-]Accept error");
-            closesocket(server_sock);
+            perror("[-] Accept error");
+            closesocket(serverSock);
             WSACleanup();
-            exit(1);
+            exit(EXIT_FAILURE);
         }
-        printf("[+] Client connected. \n");
+        printf("[+] Client connected.\n");
 
         memset(buffer, 0, sizeof(buffer));
-
-        recv(client_sock, buffer, sizeof(buffer), 0);
+        recv(clientSock, buffer, sizeof(buffer), 0);
         printf("Client: %s\n", buffer);
-
-        memset(buffer, 0, sizeof(buffer));
 
         strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
         printf("Server: %s\n", buffer);
-        send(client_sock, buffer, sizeof(buffer), 0);
+        send(clientSock, buffer, sizeof(buffer), 0);
 
-        closesocket(client_sock); // Close the client socket when done
+        closesocket(clientSock);
         printf("[+] Client disconnected.\n\n");
     }
 
-    closesocket(server_sock);
+    closesocket(serverSock);
     WSACleanup();
 
     return 0;
